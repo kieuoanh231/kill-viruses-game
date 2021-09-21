f9f0ed75790b2888931d18f46a41e1734d2a9ab7
baongoc156 <phbaongoc2001@gmail.com>
2021-09-21 14:48:56 +0700

    design

---
+++ public/js/canvas.js
@@ -0,0 +1,7 @@
+const canvas = document.querySelector('.game');
+const c = canvas.getContext('2d');
+var img = new Image();
+img.onload = function() {
+    c.drawImage(img, 50, 50,50,50);
+};
+img.src = './public/images/corona1.png';
