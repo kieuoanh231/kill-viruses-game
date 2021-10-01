a1086b0e68d03b081c8202caf026446175169217
baongoc156 <phbaongoc2001@gmail.com>
2021-10-01 10:30:32 +0700

    ngoccute

--- public/js/canvas.js
+++ public/js/canvas.js
@@ -1,24 +1,24 @@
 const mRandom = (min, max) => {
   return Math.floor(Math.random() * (max - min) + min);
 };
-
+//tạo corona
 const createCorona = () => {
   let rdIndex = mRandom(0, characters.length);
   let character = characters[rdIndex];
   characters.splice(rdIndex, 1);
   let percent = mRandom(0, 100);
-  // if (percent > 20) {
-  //   arrLength += 1;
-  //   let image = imgArray[mRandom(0, imgArray.length)];
-  //   arrCorona.push(new Corona(character, image));
-  // } else 
-  if (percent > 50) {
+  if (percent > 20) {
+    arrLength += 1;
+    let image = imgArray[mRandom(0, imgArray.length)];
+    arrCorona.push(new Corona(character, image));
+  } else if (percent > 15) {
     arrCorona.push(new Corona(character, coronaBlack, TYPE_BLACK));
   } else {
     arrLength += 1;
     arrCorona.push(new Corona(character, coronaStar, TYPE_STAR));
   }
 };
+//xoá corona
 const removeCorona = (corona) => {
   let coronaIndex = arrCorona.indexOf(corona);
   characters.push(corona.character);
@@ -26,28 +26,31 @@ const removeCorona = (corona) => {
 };
 const drawCoronas = () => {
   for (let covid of arrCorona) {
-    checkLose(covid);
+    checkWhenCoronaTouchBottom(covid);
     isCurrentCorona = covid == currentCorona ? true : false;
     covid.draw(isCurrentCorona);
     covid.update();
   }
 };
+// vẽ thông báo
 const drawNotification = () => {
   let checkAnimation = true;
-  if (checkAnimation) {
-    let y = lastCompleteY - opacity;
-    if (y > lastCompleteY - 20) {
-      ctx.font = "500 20px Poppins";
-      ctx.textAlign = "center";
-      ctx.textBaseline = "middle";
-      ctx.fillStyle = "white";
-      ctx.fillText(`+1`, lastCompleteX, y);
-      opacity = opacity + 0.5;
-    } else {
-      checkAnimation = false;
-      opacity = 0;
-      lastCompleteY = undefined;
-      lastCompleteX = undefined;
+  if (coronaBackup != undefined) {
+    const text = coronaBackup.type == TYPE_STAR ? "+2" : "+1";
+    if (checkAnimation) {
+      let y = coronaBackup.y - opacity;
+      if (y > coronaBackup.y - 20) {
+        ctx.font = "500 20px Poppins";
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        ctx.fillStyle = "white";
+        ctx.fillText(text, coronaBackup.x, y);
+        opacity = opacity + 0.5;
+      } else {
+        checkAnimation = false;
+        opacity = 0;
+        coronaBackup = undefined;
+      }
     }
   }
 };
@@ -67,41 +70,130 @@ const animation = () => {
     }
     // finishedWave(wave[waveIndex]);
   }
-  console.log(arrLength);
   repeatTime += 0.5;
   drawCoronas();
-  wordComplete();
+  completedAWord();
   drawScore();
   drawNotification();
   request = requestAnimationFrame(animation);
+  console.log(repeatTime);
   finishedWave(wave[waveIndex], repeatTime % 100 == 0);
   if (end) {
     end = false;
-    // setTimeout(function () {
-    //   alert("Hello");
-    // }, 1000);
     cancelAnimationFrame(request);
     btnR.style.display = "inline-block";
   }
 };
+// hoàn thành 1 wave
 const finishedWave = (coronaNumber, check) => {
   if (check) {
+    arrCorona = [];
     if (checkKill == coronaNumber && arrLength == coronaNumber) {
+      console.log(arrCorona);
       arrCorona = [];
       checkKill = 0;
       arrLength = 0;
       cancelAnimationFrame(request);
+      arrCorona = [];
+      ctx.font = "500 20px Poppins";
+      ctx.textAlign = "center";
+      ctx.textBaseline = "middle";
+      ctx.fillStyle = "white";
+      ctx.fillText("WAVE 1", canvas.height/2, canvas.height/2);
+      setTimeout(function () {
+        play();
+      }, 10000);
+      repeatTime = 0;
     }
   }
+  // animation();
+  // request= requestAnimationFrame(animation)
 };
-const checkLose = (covid) => {
+//kiểm tra corona chạm khung
+const checkWhenCoronaTouchBottom = (covid) => {
   if (covid.y > canvas.height - 30 && covid.type != TYPE_BLACK) {
     end = true;
+  } else if (covid.y > canvas.height + 30 && covid.type == TYPE_BLACK) {
+    characters.push(covid.character);
+    removeCorona(covid);
   }
 };
 const checkRound = (arr) => {};
 
-const checkCurrentCorona = () => {
+// Hoàn thành xong từ
+const completedAWord = () => {
+  // Xoá corona khi nhập đúng từ trong current corona
+  if (
+    arrCorrectLetters.length > 0 &&
+    arrCorrectLetters.join("") === Object.values(currentCorona.character)[1] &&
+    currentCorona.type != TYPE_BLACK
+  ) {
+    score += currentCorona.type == TYPE_STAR ? 2 : 1;
+    arrCorrectLetters = [];
+    coronaBackup = currentCorona;
+    removeCorona(currentCorona);
+    checkKill += 1;
+    firstLetter = undefined;
+    currentCorona = undefined;
+  }
+  // nhu tên
+  findAndRemoveCoronaWithWrongLetters();
+};
+//Tìm và xoá Corona nếu có, khi nhập sai từ trong current corona
+const findAndRemoveCoronaWithWrongLetters = () => {
+  let count = 0;
+  for (let covid of arrCorona) {
+    if (arrWrongLetters.length > 1) {
+      if (
+        Object.values(covid.character)[1] === arrWrongLetters.join("") &&
+        covid.type != TYPE_BLACK
+      ) {
+        removeCorona(covid);
+        arrWrongLetters = [];
+        coronaBackup = covid;
+        currentCorona = undefined;
+        break;
+      } else if (
+        Object.values(covid.character)[1] === arrWrongLetters.join("") &&
+        covid.type == TYPE_BLACK
+      ) {
+        end = true;
+        break;
+      } else if (
+        Object.values(covid.character)[1] != arrWrongLetters.join("") &&
+        covid.type == TYPE_BLACK
+      ) {
+        count += 1;
+      }
+    }
+  }
+  if (count == arrCorona.length) {
+    arrWrongLetters = [];
+    currentCorona = undefined;
+  }
+};
+
+// bat su kien nhan tu
+document.addEventListener("keypress", (e) => {
+  const key = e.key;
+  // chua co current corona thi set current
+  if (currentCorona === undefined) {
+    firstLetter = key;
+    selectCurrentCorona();
+  }
+  // Kiểm tra kí tự của current corona vs key
+  if (checkLetterOfCurrentCorona(arrCorrectLetters, key)) {
+    arrCorrectLetters.push(key);
+  } else {
+    arrWrongLetters = arrCorrectLetters;
+    arrWrongLetters.push(key);
+    arrCorrectLetters = [];
+    firstLetter = undefined;
+    currentCorona = undefined;
+  }
+});
+//Tìm và set current corona (đc gọi trong bắt sư kiện nhấn phím)
+const selectCurrentCorona = () => {
   for (let covid of arrCorona) {
     if (Object.values(covid.character)[1].split("")[0] === firstLetter) {
       currentCorona = covid;
@@ -109,11 +201,11 @@ const checkCurrentCorona = () => {
     }
   }
 };
-
-const matchesCurrentCorona = (correctLetters, keyPress) => {
+// Kiểm tra từ trong current corona vs key (đc gọi trong bắt sư kiện nhấn phím)
+const checkLetterOfCurrentCorona = (arrCorrectLetters, keyPress) => {
   let check = false;
   if (currentCorona != undefined) {
-    const idxToCheck = correctLetters.length;
+    const idxToCheck = arrCorrectLetters.length;
     const wordLetters = Object.values(currentCorona.character)[1].split("");
     if (keyPress === wordLetters[idxToCheck]) {
       check = true;
@@ -124,59 +216,15 @@ const matchesCurrentCorona = (correctLetters, keyPress) => {
   return check;
 };
 
-const wordComplete = () => {
-  if (
-    correctLettersTyped.length > 0 &&
-    correctLettersTyped.join("") ===
-      Object.values(currentCorona.character)[1] &&
-    currentCorona.type != TYPE_BLACK
-  ) {
-    score += currentCorona.type == TYPE_STAR ? 2 : 1;
-    correctLettersTyped = [];
-    lastCompleteX = currentCorona.x;
-    lastCompleteY = currentCorona.y;
-    removeCorona(currentCorona);
-    checkKill += 1;
-    firstLetter = undefined;
-    currentCorona = undefined;
-  } else if (
-    checkBlackCovid.length > 0 &&
-    checkBlackCovid.join("") === Object.values(currentCorona.character)[1] &&
-    currentCorona.type == TYPE_BLACK
-  ) {
-    removeCorona(currentCorona);
-    checkBlackCovid = [];
-    firstLetter = undefined;
-    currentCorona = undefined;
-    end = true;
-  }
-  for (let covid of arrCorona) {
-    if (checkBlackCovid.length > 1) {
-      if (
-        Object.values(covid.character)[1] === checkBlackCovid.join("") &&
-        covid.type != TYPE_BLACK
-      ) {
-        removeCorona(covid);
-        checkBlackCovid = [];
-        currentCorona = undefined;
-        break;
-      } else if (
-        Object.values(covid.character)[1] === checkBlackCovid.join("") &&
-        covid.type == TYPE_BLACK
-      ) {
-        end = true;
-        break;
-      }
-    }
-  }
-};
-
+//play game
 const play = () => {
+  isStart = true;
   name_game.style.display = "none";
   btn.style.display = "none";
   btnR.style.display = "none";
   animation();
 };
+//bat su kien nut reset
 btnR.addEventListener("click", (e) => {
   checkKill = 0;
   arrLength = 0;
@@ -186,31 +234,8 @@ btnR.addEventListener("click", (e) => {
   characters = data.map((e) => e);
   play();
 });
+
+//bat su kien nut play
 btn.addEventListener("click", (e) => {
   play();
-});
-
-document.addEventListener("keypress", (e) => {
-  const key = e.key;
-  console.log(key);
-  if (currentCorona === undefined) {
-    firstLetter = key;
-    checkCurrentCorona();
-  }
-  if (currentCorona.type != TYPE_BLACK) {
-    if (matchesCurrentCorona(correctLettersTyped, key)) {
-      correctLettersTyped.push(key);
-    } else {
-      correctLettersTyped = [];
-      firstLetter = undefined;
-      currentCorona = undefined;
-    }
-  } else {
-    if (matchesCurrentCorona(checkBlackCovid, key)) {
-      checkBlackCovid.push(key);
-    } else {
-      checkBlackCovid.push(key);
-      firstLetter = undefined;
-    }
-  }
 });
